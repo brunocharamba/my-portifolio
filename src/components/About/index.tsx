50ec3971649e6bcf00e52eb75156fb02d8d0711e
Bruno Charamba <brunocharamba@gmail.com>
2022-04-17 16:58:24 +0100

    Removed some ' from the code.

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -26,16 +26,18 @@ const About: React.FC<ComponentProps> = (props) => {
       <div className="mb-2 text-right">.</div>
       {/* About content */}
       <div className="text-md text-gray-600 text-justify">
-        <b className="text-md">Hi! I'm a full stack developer who searches for new and promising technologies to build and enhance my knowledge and projects that I participate.</b>
+        <b className="text-md">
+          Hi! I&apos;m a full stack developer who searches for new and promising technologies to build and enhance my knowledge and projects that I participate.
+        </b>
         <br />
         <br />
-        I'm {getYear()} years old programmer from Brazil who studied in Universidade de Pernambuco for my bachelor's degree, and since have been working and learning new
+        I&apos;m {getYear()} years old programmer from Brazil who studied in Universidade de Pernambuco for my bachelor&apos;s degree, and since have been working and learning new
         technologies related to the IT industry with no prejudice from any area: frontend, backend, mobile or whatever. <br />
-        <b>If it's cool, i'm in.</b>
+        <b>If it&apos;s cool, i&apos;m in.</b>
         <br />
         <br />
-        If you want to contact me for any reason, feel free to do it, I'm always looking for new opportunities and things to do. At the bottom you can find some links to my social
-        networks and other useful content.
+        If you want to contact me for any reason, feel free to do it, I&apos;m always looking for new opportunities and things to do. At the bottom you can find some links to my
+        social networks and other useful content.
       </div>
       {/* Button wrapper */}
       <div className="flex flex-row justify-end mt-4 gap-4">
